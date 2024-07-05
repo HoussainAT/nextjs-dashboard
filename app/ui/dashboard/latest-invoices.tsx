56ee4bbfe59c03b8214d421ab74354435656c5bb
HoussainAT <houssain209@gmail.com>
2024-07-05 15:50:37 +0200

    chapter 11 finished

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -3,7 +3,6 @@ import { lusitana } from '@/app/ui/fonts';
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
-fetchLatestInvoices
 
 
 
